805744847eb3a75a3f242f1dbaca0ea814fa416e
yubber <39871798+yubber@users.noreply.github.com>
2019-05-20 07:48:53 +0800

    Create dad.js

---
+++ sauce/dad.js
@@ -0,0 +1,8 @@
+javascript:void(function(){
+   let owo=document.getElementsByTagName("img");
+   let lien = 'https://cultofthepartyparrot.com/parrots/hd/dadparrot.gif'
+   Array.from(owo).forEach( (e)=>{
+       e.src=lien;e.alt=lien
+       console.log("image ◘í╓▐‼∟bÞ☼ed.");
+   });
+})();
