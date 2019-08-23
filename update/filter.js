ff26c9f322ecf3321f22a03bdba124392ccf6b7e
yubber <39871798+yubber@users.noreply.github.com>
2019-08-23 20:56:47 +0800

    Update filter.js
    merged with parrotsonly.js

--- update/filter.js
+++ update/filter.js
@@ -4,4 +4,11 @@ let thing = [] // replace with current list of gifs
 
 thing = stuff.filter( (e)=>thing.indexOf(e)==-1 )
 
-copy(thing)
+let neue = thing
+
+function flitter(a,b){
+  let c = Array.from(new Set([...a,...b]));
+  c = c.filter( (e)=>e.includes("/assets/")===false)
+  c = c.filter( (e)=>e.includes("flags")===false)
+  return Array.from(c)
+}
