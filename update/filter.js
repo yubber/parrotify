7a2bdc486bd4b757adb3716021e5b02937e9bac8
yubber <39871798+yubber@users.noreply.github.com>
2020-01-19 16:30:55 +0800

    added instructions

--- update/filter.js
+++ update/filter.js
@@ -1,3 +1,5 @@
+// RUN THIS ON cultofthepartyparrot.com
+
 let stuff = Array.from(document.getElementsByTagName("img")).map( (e)=>{return e.src} )
 
 let thing = [] // replace with current list of gifs
@@ -6,7 +8,7 @@ thing = stuff.filter( (e)=>thing.indexOf(e)==-1 )
 
 let neue = thing
 
-function flitter(a,b){ // call flitter(stuff, thing)
+function flitter(a,b){ // call copy(flitter(stuff, thing))
   let c = Array.from(new Set([...a,...b]));
   c = c.filter( (e)=>e.includes("/assets/")||e.includes("flags")||e.includes("/still/")===false)
   return Array.from(c)
